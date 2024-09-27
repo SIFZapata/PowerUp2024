fa16f1e00a261d232b35b6b603d1f11959b5105d
LeslieLlama <leslieundinecrooks@gmail.com>
2024-09-28 11:20:38 +1200

    Added basic character controller
    Added a character controller with climbing/gliding to the character test scene and some basic geometry

---
+++ PlayerMovement.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var gravity = Vector2(0,980)
+var slowfallGravity = Vector2(0,40)
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor() and not is_on_wall():
+		if Input.is_action_pressed("action_1"):
+			velocity += slowfallGravity * delta
+			$WingsSprite.visible = true
+		else:
+			velocity += gravity * delta
+			$WingsSprite.visible = false
+
+	# Handle jump.
+	#if Input.is_action_just_pressed("action_1") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+	
+	if is_on_wall():
+		if Input.is_action_pressed("action_1"):
+			velocity.y = JUMP_VELOCITY/2
+		else:
+			velocity += gravity * delta
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
